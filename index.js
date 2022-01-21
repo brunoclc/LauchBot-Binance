a030368acfb08b276e920a4a96061bd1291dd362
Bruno Cézar Lucena <brunoclc@live.com>
2022-01-21 19:35:57 -0300

    Update code

--- index.js
+++ index.js
@@ -6,52 +6,119 @@ const WebSocket = require('ws');
 const ws = new WebSocket("wss://stream.binance.com:9443/ws/!bookTicker");
 
 //escolha o symbol que vai monitorar - set the symbol to monitor
-const SYMBOL = 'BNBBUSD';
-const ASSET = 'BNB';
-//escolha o percentual de lucratividade - set the profit percent
-const profit = 1.01;
+
+const ASSET = 'LOKA';
+const BASE = 'USDT'
+const SYMBOL = ASSET + BASE;
+//escolha o percentual de lucratividade mínimo - set the profit percent
+const profit = 2;
 //escolha quanto quer comprar - set the quantity to buy
-const buyQty = 1;
+const buyQty = 100;
 //Data do agendamento
-const date = new Date(2022, 0, 19, 18, 01, 10)
+const date = new Date(2022, 0, 20, 09, 00, 0)
+
+//Percentual do pico para venda
+const percent = 92
+//Offset de tempo para início da venda (s)
+let timer = 15;
 
 //não mexa nestas variáveis
 let quantity = 0;
 let buyPrice = 0;
 let isBought = false;
 let wallet = {};
-let saldo = {};
+let saldoAsset = {};
+let saldoBase = {};
 let printStatus = {};
+let timerStarted = false;
+let SellTime = 0;
+let maxPrice = 0
+let lastPrice = 0
+let printError = {}
+
+let pSymbol = ''
+let pAsk = ''
+let pBid = ''
+let pBuyPrice = ''
+let pQty = ''
+let pNotional = ''
+let pTargetPrice = ''
+let pMaxPrice = ''
+let pLastPrice = ''
+let pOrder = {}
 
 
 setInterval(async () => {
 
     wallet = await api.balance()
 
-    saldo = wallet.balances.find(b => b.asset === ASSET).free
+    saldoAsset = wallet.balances.find(b => b.asset === ASSET).free
+    saldoBase = wallet.balances.find(b => b.asset === BASE).free
 
-    console.log(saldo)
+    if (timerStarted) {
+        timer --
+        if (timer < 5 || Number.isInteger(timer % 5) )console.log(timer)
+    }
+
+    print({'Symbol': pSymbol, 'Best ask': pAsk, 'Best bid': pBid, 'Buy Price': pBuyPrice, 'Qty': pQty, 'Notional': pNotional,
+        'Target Price': pTargetPrice, 'Max Price': pMaxPrice, 'Last Price': pLastPrice});
 
 }, 1000) 
 
+setInterval(() => {
 
-const job = nodeSchedule.scheduleJob(date, async () => {
+    if (pAsk > 0 && pAsk !== '' && !isBought){
+
+        isBought = true;
+
+        compra()
+
+    } else if(!isBought) console.log ('Aguardando primeiros dados do stream...')
+
+}, 100);
+
+function print(data){
+let printTemp = {...data, ...pOrder, 'saldo' : saldoAsset, 'Erro' : printError}
+    process.stdout.write('\033c');
+    //console.log(JSON.stringify(printStatus))
+    console.log(printTemp)
+        
+    //if(isBought) console.log(pOrder);
+
+}
+
+async function compra () {
     
-    isBought = true;
+    console.log('Iniciada compra...');
     //const order = await api.newQuoteOrder(SYMBOL, buyQty);
-    const order = await api.newOrder(SYMBOL, buyQty, );
-                
-    console.log(order);
+    const order = await api.newOrder(SYMBOL, buyQty);    
+
+    pOrder = order
 
     if (order.status === 'FILLED'){
         quantity = parseFloat(order.executedQty);
         buyPrice = parseFloat(order.fills[0].price);
+        console.log('Finalizada compra...');
+        timerStarted = true
+
     }
 
-    else{
-        console.log('Ordem não preenchida...')
-        console.log('STATUS: ' + order.status)
+    else if(!order.status && !printError ){
+        printError = ({'Erro':'Compra não preenchida...','STATUS:' : order.status})
+        
+    }else{
+
+        if (!printError) printError = ({'Erro' : 'Erro, ordem não executada!'})
     }
+
+}
+
+
+const job = nodeSchedule.scheduleJob(date, () => {
+    
+    isBought = true;
+
+    compra()
 
 })
 
@@ -67,33 +134,43 @@ ws.onmessage = async (event) => {
         const obj = JSON.parse(event.data);
 
         if (obj.s === SYMBOL) {
-            process.stdout.write('\033c');
-            console.log(`Symbol: ${obj.s}`);
-            console.log(`Best ask: ${obj.a}`);
-            console.log(`Best bid: ${obj.b}`);
-            console.log(`Buy Price: ${buyPrice}`);
-            console.log(`Qty: ${quantity}`);
-            console.log(`Notional: ${buyPrice * quantity}`);
-            console.log(`Target Price: ${buyPrice * profit}`);
 
-            console.log(wallet)
+            if(obj.a > maxPrice) maxPrice = obj.b
+            lastPrice = obj.b
+
+            pSymbol = obj.s
+            pAsk = obj.a
+            pBid = obj.b
+            pBuyPrice = buyPrice
+            pQty = quantity
+            pNotional = buyPrice * quantity
+            pTargetPrice = ((maxPrice * percent) / 100)
+            pMaxPrice = maxPrice
+            pLastPrice = lastPrice
 
             if (!isBought) {
                 
                 return;
             }
-            else if (quantity > 0 && parseFloat(obj.b) > (buyPrice * profit)) {
-                const order = await api.newOrder(SYMBOL, quantity, 0, 'SELL', 'MARKET');
-                if (order.status !== 'FILLED')
+            //else if (quantity > 0 && parseFloat(obj.b) > (buyPrice * profit)) {
+            else if (quantity > 0 && timer <= SellTime && lastPrice <= ((maxPrice * percent) / 100) && lastPrice >= (buyPrice * profit)) {
+                timer = timer --
+                console.log('Iniciada venda...')
+                const order = await api.newOrder(SYMBOL, parseFloat(saldoAsset).toFixed(1), 0, 'SELL', 'MARKET');
+                if (order.status !== 'FILLED'){
+                    
                     console.log(order);
+                }
                 else
-                    console.log(`Sold at ${new Date()} by ${order.fills[0].price}`);
-                process.exit(1);
+                    console.log(order);
+                    console.log(`Vendido em ${new Date()} por ${BASE} ${order.fills[0].price}`);
+                    console.log('Finalizada venda...')
+                    process.exit(1);
             }
         }
 
     } catch (err) {
-        console.error(err);
+        console.error(err.message);
         process.exit(1);
     }
 
